34ee7d96e149c2565764e2be512617eecf229c87
Reshy <reshy@protonmail.com>
2022-11-10 12:15:56 +0100

    Test fix

--- cypress/integration/farms.test.ts
+++ cypress/integration/farms.test.ts
@@ -8,16 +8,16 @@ describe('Farms', () => {
     })
   
     it('Should open farms when clicked', () => {
-        cy.get('#farms-table', { timeout: 20000 }).contains('td', 'Float').should('be.visible').click();
+        cy.get('#farms-table', { timeout: 20000 }).contains('td', 'Float').should('be.visible').click({ force: true });
         cy.get('#farms-table', { timeout: 20000 }).contains('a', 'View contract').should('be.visible');
     })
 
     it('Filters should work correctly', () => {
         cy.get('#all-select-tab').should('have.class', 'ACTIVE')
-        cy.get('#anchor-select-tab').should('be.visible').click().should('have.class', 'ACTIVE')
-        cy.get('#all-select-tab').should('not.have.class', 'ACTIVE')
-        cy.get('#float-select-tab').should('be.visible').click().should('have.class', 'ACTIVE')
-        cy.get('#anchor-select-tab').should('not.have.class', 'ACTIVE')
+        cy.get('#anchor-select-tab', { timeout: 20000 }).should('be.visible').click().should('have.class', 'ACTIVE')
+        cy.get('#all-select-tab', { timeout: 20000 }).should('not.have.class', 'ACTIVE')
+        cy.get('#float-select-tab', { timeout: 20000 }).should('be.visible').click().should('have.class', 'ACTIVE')
+        cy.get('#anchor-select-tab', { timeout: 20000 }).should('not.have.class', 'ACTIVE')
     })
 
     it('Staked only filter works', () => {

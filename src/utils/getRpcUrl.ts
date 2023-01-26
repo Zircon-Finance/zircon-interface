01913104f3ce8fc82308c4adb39ed630d27b8d3e
Reshy <reshy@protonmail.com>
2023-01-26 18:36:15 +0100

    RPC change

--- src/utils/getRpcUrl.ts
+++ src/utils/getRpcUrl.ts
@@ -1,7 +1,7 @@
 const getNodeUrl = (chainId: number) => {
   return chainId === 56 ? 'https://bsc-dataseed.binance.org' : chainId === 1287 ? 
-  'https://moonbase-alpha.public.blastapi.io' :
-   'https://moonriver.public.blastapi.io'
+  'https://moonbase-alpha.public.blastapi.io' : chainId === 1285 ?
+   'https://moonriver.public.blastapi.io' : 'https://bsc-testnet.public.blastapi.io'
 }
 
 export default getNodeUrl

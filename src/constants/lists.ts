cb849475b9da63e07543c519b1f5cfd6f4fc01f1
Reshy <reshy@protonmail.com>
2023-02-22 14:54:47 +0100

    List

--- src/constants/lists.ts
+++ src/constants/lists.ts
@@ -1,6 +1,6 @@
 // the DCTDEX Default token list lives here
 export const DEFAULT_TOKEN_LIST_URL =
-  'https://gist.githubusercontent.com/ReshyResh/1d4a52bb0f3f1f1252974cc1f34eb354/raw/feb03100138273a81874fd87266c48bc0e9dba5e/BSC_Testnet.json'
+  'https://gist.githubusercontent.com/ReshyResh/1d4a52bb0f3f1f1252974cc1f34eb354/raw/9a2ff0411ed86bb58d2a48893ee98fbf34af26e3/BSC_Testnet.json'
 
 export const PRICE_API = 'https://api.binance.com/api/v3/ticker/price?symbol='
 
